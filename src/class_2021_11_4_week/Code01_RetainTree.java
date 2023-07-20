d11d2a0b9d3a101992487fcfb0b341c3aeecc085
MoshangNiu <2551782086@qq.com>
2023-07-20 10:05:34 -0700

    feat 添加注释

--- src/class_2021_11_4_week/Code01_RetainTree.java
+++ src/class_2021_11_4_week/Code01_RetainTree.java
@@ -3,6 +3,10 @@ package class_2021_11_4_week;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 给定一棵多叉树的头节点head，每个节点只有黑白两色 所有黑节点都保留，
+ * 所有从头节点到黑节点路径上的点都保留 返回处理后树的头节点
+ */
 public class Code01_RetainTree {
 
 	public static class Node {
